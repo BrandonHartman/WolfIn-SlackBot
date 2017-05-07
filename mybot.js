d5ec29de2b0ec9f2f95bedb78204cef6d070b764
Brandon Lee Hartman <BrandonHartman@users.noreply.github.com>
2017-05-07 19:18:16 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,9 +1,10 @@
 var Bot = require('slackbots');
+var request = require('request');
 
-// Settings
+//settings
 var settings = {
 
-     token: 'oxb-179102533538-14z5DTAaWiHyDnqY9mivOajP',
+     token: 'xoxb-179102533538-14z5DTAaWiHyDnqY9mivOajP',
      name: 'WolfIn'
 };
 
@@ -34,10 +35,17 @@ bot.on('message',  function(message){
             			switch(commandType)
             			{
             				case 'drawchart':
-            								   console.log('attempting to get chart answer');
+            								   console.log('attempting to get chart answer...');
                                                var answer = getChartAnswer(command);
                                                bot.postMessageToChannel(channel, answer);
             				                   break;
+            				case 'showgitstats':
+            									console.log('attempting to show Github stats...');
+                                              getGitStatsAnswer(command);
+                                               // var answer = getGitsStatsAnswer(command);
+                                               // bot.postMessageToChannel(channel, answer);
+            				                   break;
+
             				default:
                                      bot.postMessageToChannel(channel, "Hey there! Welcome to the Slack Channel.");
                                      break;
@@ -48,13 +56,17 @@ bot.on('message',  function(message){
             		
             	}
             }
-               
+      
 
-     });
-function getCommandType(command)    
+     }); 
+
+
+ function getCommandType(command)    
  {
     if(command.indexOf('draw chart') > - 1)  
     	return "drawchart";
+    if(command.indexOf('show github top contributor stats') > - 1)  
+    	return "showgitstats";
 
 
  }
@@ -62,7 +74,10 @@ function getCommandType(command)
 
  function getChartAnswer(command)
  {
-      //command : '@WolfIn draw chart type:pi data:{60,40,20} legends:{USA,UK,Russia}';
+      // command : '@wolfin draw chart type:pi data:{60,40,20} legends:{USA,UK,Russia}';
+      // command : '@wolfin draw chart type:line data:{60,40,20} legends:{USA,UK,Russia}';
+      // command : '@wolfin draw chart type:bar data:{60,40,20} legends:{USA,UK,Russia}';
+
       var answer = "";
       var data = command.split(/ /i);
       var chartType = data[3].split(/:/i)[1];
@@ -91,5 +106,65 @@ function getCommandType(command)
       //Chart URL: https://chart.googleapis.com/chart?cht=p3&chs=250x100&chd=t:60,40,20&chl=USA|UK|Russia    
 
       return answer;
+
+ }
+ var options = {
+
+        url: https: 'api.github.com/users/BrandonHartman/repos',
+        headers: {
+              'User-Agent':'request'
+        }
+ };
+
+ function getGitStatsAnswer(command)
+ {
+      //command : '@wolfin show github top contributor stats repo:facebook/stats';
+
+      var answer = "";
+      var data = command.split(/ /i);
+      console.log("data is " + data);
+      var repo = data[6].split(/:/i)[1];
+      console.log("repo is " + repo);
+      console.log("inside git answer");
+      console.log("url before " + options.url );
+      var localUrl = options.url.replace('{0}', repo);
+      console.log("url after " + localUrl );
+
+      options = {
+        url: localUrl,
+        headers: {
+          'User-Agent':'request'
+                 }
+      
+        };  
+      request(options, function(error, response, body)
+                    {
+                       console.log(response) // Show the HTML for the Google homepage. 
+                var profile = JSON.parse(response.body);
+                // var profile = response;
+                console.log("profile2 " + profile);
+                var cntr=0;
+                for (var myKey in profile)
+                { 
+                  console.log(profile[myKey].avatar_url, profile[myKey].contributions,profile[myKey].login); 
+                    var st = 'Name-{0} Contributions-{1}  image- {2}';
+                    st = st.replace('{0}', profile[myKey].login).replace('{1}',profile[myKey].contributions).replace('{2}',profile[myKey].avatar_url) ;    
+                  console.log(st);
+                  bot.postMessageToChannel(channel, st);
+                  cntr++;
+                  if(cntr >= 5)
+                  {
+                    break;
+                  }
+                }
+
+                    }
+
+            );
+
+
+
+     
+      
 
  }
