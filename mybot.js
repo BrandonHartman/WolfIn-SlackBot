b5a494c92bb4a9c2e18831c03dde596cbffae225
Brandon Lee Hartman <BrandonHartman@users.noreply.github.com>
2017-05-07 05:42:18 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -13,7 +13,7 @@ var bot = new Bot(settings);
 
 bot.on('start', function(){
                
-               bot.postMessageToChannel(channel, 'Hello Channel. I am connected to the channel!');
+               bot.postMessageToChannel(channel, 'Greetings! I am successfuly connected to the channel.');
 
               }
      );
@@ -29,10 +29,67 @@ bot.on('message',  function(message){
             		console.log(message.text.toLowerCase());
             		if(message.text.toLowerCase().indexOf('@u5930fpfu') > -1 )
             		{
-            			bot.postMessageToChannel(channel, "Hey there! Welcome to the slack channel!");
+            			var command = message.text.toLowerCase();
+            			var commandType = getCommandType(command);
+            			switch(commandType)
+            			{
+            				case 'drawchart':
+            								   console.log('attempting to get chart answer');
+                                               var answer = getChartAnswer(command);
+                                               bot.postMessageToChannel(channel, answer);
+            				                   break;
+            				default:
+                                     bot.postMessageToChannel(channel, "Hey there! Welcome to the Slack Channel.");
+                                     break;
+
+            			}
+            			
             		}
+            		
             	}
             }
                
 
-     });     
+     });
+function getCommandType(command)    
+ {
+    if(command.indexOf('draw chart') > - 1)  
+    	return "drawchart";
+
+
+ }
+
+
+ function getChartAnswer(command)
+ {
+      //command : '@funbot draw chart type:pi data:{60,40,20} legends:{USA,UK,Russia}';
+      var answer = "";
+      var data = command.split(/ /i);
+      var chartType = data[3].split(/:/i)[1];
+      switch(chartType)
+      {
+          case 'pi':
+                    chartType = 'p3';
+                    break; 
+          case 'line':
+                    chartType = 'lc';
+                    break; 
+          case 'bar':
+                    chartType = 'bvg';
+                    break; 
+
+      }
+      console.log('chartType is ' + chartType);
+      var chartData = data[4].split(/:/i)[1].replace('{','').replace('}','');
+      console.log('chartData is ' + chartData);
+      var legends = data[5].split(/:/i)[1].replace('{','').replace('}','').replace(/,/gi,'|');
+	  console.log('legends is ' + legends);
+      var urlFormat = 'https://chart.googleapis.com/chart?cht={0}&chs=250x100&chd=t:{1}&chl={2}';
+
+      answer = urlFormat.replace('{0}',chartType).replace('{1}', chartData).replace('{2}', legends);
+      console.log('answer is ' + answer);
+      //Chart URL: https://chart.googleapis.com/chart?cht=p3&chs=250x100&chd=t:60,40,20&chl=USA|UK|Russia    
+
+      return answer;
+
+ }
